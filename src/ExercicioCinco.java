66cf15b6b2af3c3c7caee51538b6aa3fd93fcda1
BrenoJefferson-GabrielVitor <BrenoJefferson-GabrielVItor@test.com>
2022-03-28 10:13:29 -0300

    Desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,18 @@
+
+public class ExercicioCinco {
+	public static void main(String[] args) {
+		double trabalhoDeHistoria = 8.0;
+		double trabalhoDeMatematica = 7.0;
+		double trabalhoDeCiencia = 10;
+		double trabalhoDeGeografia = 9.5;
+		
+		double med;
+		
+		med = (trabalhoDeCiencia + trabalhoDeGeografia + trabalhoDeHistoria + trabalhoDeMatematica) /4;
+		System.out.printf("%.2f", med);
+	}
+	
+	
+	
+
+}
