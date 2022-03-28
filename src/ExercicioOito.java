de147da7be1332520d087d0fe14404a95cdfe284
BrenoJefferson-GabrielVitor <BrenoJefferson-GabrielVItor@test.com>
2022-03-28 10:42:18 -0300

    Desenvolvimento do exercicio 8

---
+++ src/ExercicioOito.java
@@ -0,0 +1,13 @@
+
+public class ExercicioOito {
+	public static void main(String[] args) {
+		String diaSemana = "sabado";
+		
+		if (diaSemana == "sabado" || diaSemana == "domingo") {
+			System.out.println("O banco está fechado, tente outro dia");
+		}
+		else {
+			System.out.println("Você pode ir ao banco");
+		}
+	}
+}
