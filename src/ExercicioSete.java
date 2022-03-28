134be8ce47b516e09bc179507ada82bae4a1ce72
BrenoJefferson-GabrielVitor <BrenoJefferson-GabrielVItor@test.com>
2022-03-28 10:18:54 -0300

    Desenvolvimento do exercicio 6

---
+++ src/ExercicioSete.java
@@ -0,0 +1,16 @@
+
+public class ExercicioSete {
+	public static void main(String[] args) {
+		double peso = 3;
+		
+		if (peso < 4) {
+			System.out.println("Abaixo do Peso");
+		}
+		else if (peso > 10) {
+			System.out.println("Acima do Peso");
+		}
+		else {
+			System.out.println("Peso normal");
+		}
+	}
+}
